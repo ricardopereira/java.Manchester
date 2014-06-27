452c81fa8328628e6d7ea7667181f544169649c9
Krupel <gncserrano@gmail.com>
2014-06-27 17:30:48 +0100

    logic

--- Manchester/src/main/java/logic/Triagem.java
+++ Manchester/src/main/java/logic/Triagem.java
@@ -1,5 +1,33 @@
 package logic;
 
-public class Triagem {
+import pt.manchester.states.IEstados;
 
+public class Triagem {
+	
+	private IEstados estadoActual;
+    private IEstados estadoAnterior;
+
+    public Triagem() {};
+    
+    public void setEstado(IEstados estado)
+    {
+        this.setEstadoAnterior(this.getEstadoActual());
+        this.setEstadoActual(estado);
+    }
+
+	public IEstados getEstadoAnterior() {
+		return estadoAnterior;
+	}
+
+	public void setEstadoAnterior(IEstados estadoAnterior) {
+		this.estadoAnterior = estadoAnterior;
+	}
+
+	public IEstados getEstadoActual() {
+		return estadoActual;
+	}
+
+	public void setEstadoActual(IEstados estadoActual) {
+		this.estadoActual = estadoActual;
+	}
 }
