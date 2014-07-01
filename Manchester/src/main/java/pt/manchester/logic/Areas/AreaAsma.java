82de1162e4a26322070955cc219f5d170891c666
Ricardo Pereira <ricardopereira.eu@gmail.com>
2014-07-02 00:41:38 +0100

    Closed #2 - Asma

--- Manchester/src/main/java/pt/manchester/logic/Areas/AreaAsma.java
+++ Manchester/src/main/java/pt/manchester/logic/Areas/AreaAsma.java
@@ -19,19 +19,45 @@ public class AreaAsma extends Area {
 		
 		opcoes.add("Compromisso da via aérea");
 		opcoes.add("Respiração ineficaz");
+		opcoes.add("Criança não reactiva");
+		opcoes.add("Choque");
+		
 		setOpcoesVermelhas(opcoes);
 	}
 
 	@Override
 	protected void preencheLaranja() {
+		ArrayList<String> opcoes = new ArrayList<>();
+		
+		opcoes.add("Incapacidade de articular frases completas");
+		opcoes.add("Taquicardia acentuada");
+		opcoes.add("PEFR muito baixo");
+		opcoes.add("SAO2 muito baixo");
+		opcoes.add("Alteração do estado de consciência");
+		
+		setOpcoesLaranja(opcoes);
 	}
 
 	@Override
 	protected void preencheAmarelo() {
+		ArrayList<String> opcoes = new ArrayList<>();
+		
+		opcoes.add("PEFR baixo");
+		opcoes.add("SAO2 baixo");
+		opcoes.add("História significativa de asma");
+		opcoes.add("Asma sem melhoria com o seu tratamento habitual");
+		
+		setOpcoesAmarelo(opcoes);
 	}
 
 	@Override
 	protected void preencheVerde() {
+		ArrayList<String> opcoes = new ArrayList<>();
+		
+		opcoes.add("Broncospasmo");
+		opcoes.add("Provável infecção respiratória");
+		
+		setOpcoesVerde(opcoes);
 	}
 
 }
